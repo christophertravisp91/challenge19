85df40cb17c3df3ac905888dddbdbff3ddabe611
TomFoolery <christophertravisp91@gmail.com>
2022-04-03 12:44:18 -0400

    finished service worker

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -3,7 +3,7 @@ const request = indexedDB.open('budget_tracker',1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+    db.createObjectStore('new_budgetItem', { autoIncrement: true });
 };
 
 request.onsuccess = function(event) {
@@ -20,9 +20,49 @@ request.onerror = function(event) {
 
 function saveRecord(record) {
     
-    const transaction = db.transaction(['new_pizza'], 'readwrite');
+    const transaction = db.transaction(['new_budgetItem'], 'readwrite');
 
-    const pizzaObjectStore = transaction.objectStore('new_pizza');
+    const budgetItemObjectStore = transaction.objectStore('new_budgetItem');
 
-    pizzaObjectStore.add(record);
+    budgetItemObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_budgetItem'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budgetItem');
+
+    const getAll = budgetItemObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_budgetItem'], 'readwrite');
+
+                    const budgetItemObjectStore = transaction.objectStore('new_budgetItem');
+
+                    budgetItemObjectStore.clear();
+
+                    alert('All saved transactions has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
